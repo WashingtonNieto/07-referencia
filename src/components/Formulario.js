0e76c706bd90eb6578f3a740beb332cc2b92c689
Washington Nieto <washington_nieto@hotmail.com>
2025-02-02 15:53:22 -0500

    ejemplo hoot useRef

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,13 +2,22 @@ import React, { useRef } from 'react'
 
 export const Formulario = () => {
 
-  const nombre = useRef("Washington");
+  const nombreInput = useRef();
+  const apellidoInput = useRef();
+  const emailInput = useRef();
+  const miCaja = useRef();
 
   const mostrar = e => {
     e.preventDefault();
 
-    console.log(nombre);
+    console.log(nombreInput.current.value);
+    console.log(apellidoInput.current.value);
+    console.log(emailInput.current.value);
 
+    //mi caja
+    let { current: caja} = miCaja;
+    caja.classList.add("fondoVerde");
+    caja.innerHTML = "Formulario Enviado!"
   }
 
 
@@ -16,10 +25,14 @@ export const Formulario = () => {
     <div>
       <h1>Formulario</h1>
 
-      <form className='contact' >
-        <input type='text' placeholder='Nombre'/> <br/>
-        <input type='text' placeholder='Apellido'/><br/>
-        <input type='text' placeholder='Email'/><br/>
+      <div ref={miCaja} className='miCaja'>
+        <h2>Pruebas con useRef</h2>
+      </div>
+
+      <form onSubmit={mostrar} >
+        <input type='text' placeholder='Nombre' ref={nombreInput}/> <br/>
+        <input type='text' placeholder='Apellido' ref={apellidoInput}/><br/>
+        <input type='text' placeholder='Email' ref={emailInput}/><br/>
         <textarea type='text' placeholder='Motivo de contacto'/><br/>
         <input type='submit' value='Enviar'/>
       </form>
